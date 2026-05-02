a05f42fcf884a65d4f24893f820fbe778e7adf34
yogesh17kumar <yogeshkumar17082003@gmail.com>
2026-05-02 20:36:15 +0530

    Fixed MongoDB connection

--- server/server.js
+++ server/server.js
@@ -51,10 +51,7 @@ app.get("/", (req, res) => {
 });
 
 // MongoDB Connect
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log("MongoDB Error:", err));
 // Server Start
