a68a1812c6c8f3ed16a132b42f9153526ac9b74d
yogesh17kumar <yogeshkumar17082003@gmail.com>
2026-05-02 20:30:24 +0530

    Added MongoDB debug

--- server/server.js
+++ server/server.js
@@ -51,10 +51,12 @@ app.get("/", (req, res) => {
 });
 
 // MongoDB Connect
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 .then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
+.catch(err => console.log("MongoDB Error:", err));
 // Server Start
 app.listen(process.env.PORT, () => {
     console.log(`Server Running on Port ${process.env.PORT}`);
